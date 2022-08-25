2ce5907e87f6f02be16d51a824dac324f2228e16
Yineteili <65248015+Yineteili@users.noreply.github.com>
2022-08-25 10:41:36 +0000

    Update GameManager.cs

--- GameManager.cs
+++ GameManager.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using TMPro;
 public class GameManager : MonoBehaviour
 {
+    public TMP_InputField letterInput;
+
+    public string[] wordlist;
     // Start is called before the first frame update
     void Start()
     {
